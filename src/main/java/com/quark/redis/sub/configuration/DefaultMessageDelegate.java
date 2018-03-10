09bfaa3d525a3d174f2f493278023b371c679799
looper123 <m17701848923@163.com>
2018-03-10 10:18:07 +0800

    repair

--- src/main/java/com/quark/redis/sub/configuration/DefaultMessageDelegate.java
+++ src/main/java/com/quark/redis/sub/configuration/DefaultMessageDelegate.java
@@ -8,32 +8,33 @@ import java.util.Map;
 
 /**
  * Created by ZhenpengLu on 2018/3/9.
+ * 消息委托类  书写拿到消息后自定义的操作
  */
 @Component
 public class DefaultMessageDelegate implements MessageDelegate {
 
     @Override
     public void handleMessage(String message) {
-        System.out.println(message+"message");
+        System.out.println(message+"---this is a string message");
     }
 
     @Override
     public void handleMessage(Map message) {
-        System.out.println(message+"message");
+        System.out.println(message+"----this is a map message");
     }
 
     @Override
     public void handleMessage(byte[] message) {
-        System.out.println(message+"message");
+        System.out.println(message+"----this is a byte[] message");
     }
 
     @Override
     public void handleMessage(Serializable message) {
-        System.out.println(message+"message");
+        System.out.println(message+"----this is a serializable message");
     }
 
     @Override
     public void handleMessage(Serializable message, String channel) {
-        System.out.println(message+"message");
+        System.out.println(message+"----this is a serializable message  from channel---"+channel);
     }
 }
--- src/main/java/com/quark/redis/sub/configuration/MessageDelegate.java
+++ src/main/java/com/quark/redis/sub/configuration/MessageDelegate.java
@@ -5,11 +5,17 @@ import java.util.Map;
 
 /**
  * Created by ZhenpengLu on 2018/3/9.
+ * 消息委托顶层接口
  */
 public interface MessageDelegate {
     void handleMessage(String message);
-    void handleMessage(Map message); void handleMessage(byte[] message);
+
+    void handleMessage(Map message);
+
+    void handleMessage(byte[] message);
+
     void handleMessage(Serializable message);
+
     // pass the channel/pattern as well
     void handleMessage(Serializable message, String channel);
 }
