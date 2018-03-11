0b7638cc5a8605fa0cf66747fd62f2c493b4e801
looper123 <m17701848923@163.com>
2018-03-11 17:30:50 +0800

    Update README.cmd

--- redis-sentinel-distribute/README.cmd
+++ redis-sentinel-distribute/README.cmd
@@ -1,5 +1,4 @@
-环境搭建
-======
+#环境搭建
 一个两台redis哨兵(30、32) 每一个哨兵各有三台redis实例（一主两从）
 具体sentinel和master、slave 的配置 ：参考source/server_30/config & source/server_32/config
 启动脚本：参考source/server_30/scripts & source/server_32/scripts   其中sentinel_start.sh 是整个sentinel环境的启动脚本
